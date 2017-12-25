593d7d1c222c892ba9c9be63f39839a59968e849
Botasky <291770865@qq.com>
2017-12-25 18:38:03 +0800

    updtae gradle

---
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.sirius.cybird.ui.base;
+
+import com.trello.rxlifecycle2.components.RxActivity;
+
+/**
+ * Description：
+ * Created by 苏良波 on 2017/12/25.
+ */
+
+public class BaseActivity extends RxActivity {
+}
