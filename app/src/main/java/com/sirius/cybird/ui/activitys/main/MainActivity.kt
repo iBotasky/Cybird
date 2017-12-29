dc97bb7c8b7802c72d0a1f0fdc771c4b1158feb6
Botasky <291770865@qq.com>
2017-12-29 20:57:05 +0800

    update for rxjava transform

--- app/src/main/java/com/sirius/cybird/ui/activitys/main/MainActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/activitys/main/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.sirius.cybird.ui.activitys.main
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.os.PersistableBundle
 import android.util.Log
 import com.sirius.cybird.R
 import com.sirius.cybird.databinding.ActivityMainBinding
 import com.sirius.cybird.net.api.FilmsApi
-import com.sirius.cybird.net.api.GirlsApi
+import com.sirius.cybird.ui.base.BaseActivity
+import com.trello.rxlifecycle2.RxLifecycle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,31 +15,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 /**
  * Create by Botasky
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val viewModel = MainViewModel("Botasky")
-        mBinding.viewModel = viewModel
-        mBinding.executePendingBindings()
-
-//        button.setOnClickListener({
-//            GirlsApi.getGirlsService()
-//                    .accessGirls(1)
-//                    .subscribeOn(Schedulers.io())
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(
-//                            { girls -> viewModel.name = girls.results[0].url },
-//                            { error -> Log.e("TAG", error.message) },
-//                            { Log.e("TAG", "onComplete")}
-//                    )
-//        })
+        mBinding?.viewModel = viewModel
+        mBinding?.executePendingBindings()
 
         button.setOnClickListener({
             FilmsApi.getFilmsService()
                     .getComingSoon(0, 10)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .compose(bindToLifecycle())
                     .subscribe({ filmsData ->
                         viewModel.name = filmsData.title
                     }, { e ->
@@ -48,5 +40,8 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    override fun getLayoutResource(): Int {
+        return R.layout.activity_main
+    }
 
 }
--- app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
@@ -1,10 +1,27 @@
 package com.sirius.cybird.ui.base
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.sirius.cybird.R
 import com.trello.rxlifecycle2.components.RxActivity
 
 /**
  * Description：
  * Created by Botasky on 2017/12/26.
  */
-class BaseActivity : RxActivity() {
+open class BaseActivity<M : ViewDataBinding> : RxActivity() {
+    var mBinding: M? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, getLayoutResource())
+    }
+
+    open fun getLayoutResource(): Int {
+        return R.layout.activity_main
+    }
+
+
 }
