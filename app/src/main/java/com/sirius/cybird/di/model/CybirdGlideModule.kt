6c81638c51f9c4419d5f8d721c908c0e2441b794
Botasky <291770865@qq.com>
2018-01-14 12:05:27 +0800

    update for Glide and gradlew

--- app/src/main/java/com/sirius/cybird/di/model/CybirdGlideModule.kt
+++ app/src/main/java/com/sirius/cybird/di/model/CybirdGlideModule.kt
@@ -1,11 +1,16 @@
 package com.sirius.cybird.di.model
 
 import android.content.Context
+import android.os.Environment
+import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
 import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
+
 
 /**
  *
@@ -23,7 +28,24 @@ class CybirdGlideModule : AppGlideModule() {
         //硬盘缓存
         val MAX_DISK_CACHE_SIZE = 1024 * 1024 * 100 //100MB
         val CACHE_FILE_NAME = "cybirdImageCache"
-        builder?.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, CACHE_FILE_NAME, MAX_DISK_CACHE_SIZE.toLong()))
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            val downloadDirectoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                    CACHE_FILE_NAME
+            //路径---->sdcard/imgCache
+            builder?.setDiskCache(DiskLruCacheFactory(downloadDirectoryPath, MAX_DISK_CACHE_SIZE.toLong()))
+        } else {
+            //路径---->/sdcard/Android/data/<application package>/cache/imgCache
+            builder?.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, CACHE_FILE_NAME, MAX_DISK_CACHE_SIZE.toLong()))
+        }
 
+    }
+
+    override fun registerComponents(context: Context?, glide: Glide?, registry: Registry?) {
+        super.registerComponents(context, glide, registry)
+    }
+
+    override fun isManifestParsingEnabled(): Boolean {
+        //Glide3是用AndroidMainfest做注册
+        return false
     }
 }
