b99460199e02eaaa32faeb2d0101df38668088e5
Botasky <291770865@qq.com>
2018-01-26 10:50:18 +0800

    update for SP

--- app/src/main/java/com/sirius/cybird/CybirdApplication.kt
+++ app/src/main/java/com/sirius/cybird/CybirdApplication.kt
@@ -1,6 +1,7 @@
 package com.sirius.cybird
 
 import android.app.Application
+import android.content.Context
 import com.facebook.stetho.Stetho
 import com.sirius.cybird.db.MyObjectBox
 import io.objectbox.BoxStore
@@ -10,11 +11,21 @@ import io.objectbox.BoxStore
  *Create By Botasky 20/01/2018
  **/
 class CybirdApplication : Application() {
+    companion object {
+        lateinit var mAppContext : CybirdApplication
+
+        fun getContext() : Context {
+            return mAppContext.applicationContext
+        }
+    }
+
+
     lateinit var mBoxStore : BoxStore
         set
 
     override fun onCreate() {
         super.onCreate()
+        mAppContext = this
         setupStetho()
         setupObjectBox()
     }
