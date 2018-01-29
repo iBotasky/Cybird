f6ad7ad3b22a80059d799465ba9af6e279a5cdcb
Botasky <291770865@qq.com>
2018-01-29 11:15:19 +0800

    update for splash

--- app/src/main/java/com/sirius/cybird/ui/activitys/splash/SplashActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/activitys/splash/SplashActivity.kt
@@ -1,14 +1,16 @@
 package com.sirius.cybird.ui.activitys.splash
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import com.sirius.cybird.R
+import com.sirius.cybird.databinding.ActivitySplashBinding
 import com.sirius.cybird.ui.activitys.main.MainActivity
+import com.sirius.cybird.ui.base.BaseActivity
 import java.lang.ref.WeakReference
 
-class SplashActivity : Activity() {
+class SplashActivity : BaseActivity<ActivitySplashBinding>() {
     companion object {
         private val SPLASH_DELAY_MILLIONS: Long = 10000
         private val SPLASH_GO_HOME = 1000
@@ -37,10 +39,15 @@ class SplashActivity : Activity() {
      */
     lateinit var mHandler: Handler
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mHandler = SplashHandler(this)
         mHandler.sendEmptyMessageDelayed(SPLASH_GO_HOME, SPLASH_DELAY_MILLIONS)
+    }
+
+    override fun getLayoutResource(): Int {
+        return R.layout.activity_splash
     }
 
     fun goHome() {
--- app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
@@ -3,14 +3,13 @@ package com.sirius.cybird.ui.base
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.os.Bundle
-import com.sirius.cybird.R
-import com.trello.rxlifecycle2.components.RxActivity
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
 /**
  * Description：
  * Created by Botasky on 2017/12/26.
  */
-open class BaseActivity<M : ViewDataBinding> : RxActivity() {
+open class BaseActivity<M : ViewDataBinding> : RxAppCompatActivity() {
     var mBinding: M? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
