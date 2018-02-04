d22b89d35b87f3b90be76764a61d098525df4623
Botasky <291770865@qq.com>
2018-02-04 17:01:20 +0800

    update for systembar

--- app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.sirius.cybird.ui.base
 
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.os.Build
 import android.os.Bundle
+import com.flyco.systembar.SystemBarHelper
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
 /**
@@ -13,9 +15,13 @@ open abstract class BaseActivity : RxAppCompatActivity() {
 
     lateinit var mBinding: ViewDataBinding
 
+    var mIsDark = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, getLayoutResource())
+
+        setStatusBar()
     }
 
     fun <V : ViewDataBinding> getBaseViewBinding(): V {
@@ -24,4 +30,44 @@ open abstract class BaseActivity : RxAppCompatActivity() {
 
     abstract fun getLayoutResource(): Int
 
+
+    fun isDark(): Boolean {
+        return mIsDark
+    }
+
+    fun isImmersiveStatusBar(): Boolean {
+        return false
+    }
+
+    fun getImmersiveStatusBarAlpha(): Float {
+        if (isDark()) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                if (SystemBarHelper.isMIUI6Later() || SystemBarHelper.isFlyme4Later() || Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                } else {
+                    return 0.3f
+                }
+            }
+        }
+
+        return 0f
+    }
+
+    protected fun setStatusBar() {
+        if (isDark()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                SystemBarHelper.setStatusBarDarkMode(this)
+            }
+        }
+
+        if (!isImmersiveStatusBar()) {
+            return
+        }
+
+        SystemBarHelper.immersiveStatusBar(this, getImmersiveStatusBarAlpha())
+
+//        if (mToolbar != null) {
+//            SystemBarHelper.setHeightAndPadding(this, mToolbar)
+//        }
+    }
 }
