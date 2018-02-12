7178ce979aca53449f04f4b5f77f0b216b164a46
Botasky <291770865@qq.com>
2018-02-12 12:33:50 +0800

    update for multiState view

--- app/src/main/java/com/sirius/cybird/ui/base/BaseRecyclerFragment.kt
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseRecyclerFragment.kt
@@ -1,19 +1,33 @@
 package com.sirius.cybird.ui.base
 
+import android.support.annotation.LayoutRes
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.kennyc.view.MultiStateView
 import com.sirius.cybird.R
+import com.sirius.cybird.utils.ToastUtils
 
 abstract class BaseRecyclerFragment : BaseLazyFragment(), SwipeRefreshLayout.OnRefreshListener {
     lateinit var mRecyclerView: RecyclerView
     lateinit var mSwipeRefresh: SwipeRefreshLayout
+    lateinit var mMultiStateView: MultiStateView
+    lateinit var mMultiStateErrorRetry: View
 
 
     override fun setupViews() {
         super.setupViews()
-        mRecyclerView = mBinding.root.findViewById(R.id.id_recycler_view)
-        mSwipeRefresh = mBinding.root.findViewById(R.id.id_swipe_refresh)
+        mMultiStateView = mBinding.root.findViewById(R.id.id_multi_state_view)
+        mRecyclerView = mMultiStateView.findViewById(R.id.id_recycler_view)
+        mSwipeRefresh = mMultiStateView.findViewById(R.id.id_swipe_refresh)
+        mMultiStateView.setViewForState(getMultiStateViewEmpty(), MultiStateView.VIEW_STATE_EMPTY)
+        mMultiStateView.setViewForState(getMultiStateViewError(), MultiStateView.VIEW_STATE_ERROR)
+        mMultiStateView.setViewForState(getMultiStateViewLoading(), MultiStateView.VIEW_STATE_LOADING)
+
+        mMultiStateErrorRetry = mMultiStateView.getView(MultiStateView.VIEW_STATE_ERROR)!!.findViewById(R.id.id_multi_state_error_retry)
+        mMultiStateErrorRetry.setOnClickListener { loadData() }
+
         mSwipeRefresh.setColorSchemeColors(*getSwipeRefreshColorSchemeRes())
 
         mRecyclerView.layoutManager = getRecyclerManager()
@@ -34,6 +48,25 @@ abstract class BaseRecyclerFragment : BaseLazyFragment(), SwipeRefreshLayout.OnR
 
     override fun onRefresh() {
         loadData()
+    }
+
+    @LayoutRes
+    open fun getMultiStateViewEmpty():Int{
+        return R.layout.state_empty_view
+    }
+
+    @LayoutRes
+    open fun getMultiStateViewError():Int{
+        return R.layout.state_error_view
+    }
+
+    @LayoutRes
+    open fun getMultiStateViewLoading():Int{
+        return R.layout.state_loading_view
+    }
+
+    open fun setOnRetry(retry:()->Unit){
+        mMultiStateErrorRetry.setOnClickListener { retry() }
     }
 
 }
