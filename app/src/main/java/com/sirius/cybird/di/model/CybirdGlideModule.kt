3329407f72d0f74bcad27dd0cdfea761d07e7348
Botasky <291770865@qq.com>
2018-02-23 15:50:41 +0800

    update for glide default request options

--- app/src/main/java/com/sirius/cybird/di/model/CybirdGlideModule.kt
+++ app/src/main/java/com/sirius/cybird/di/model/CybirdGlideModule.kt
@@ -6,10 +6,12 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
 import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 
 
 /**
@@ -19,11 +21,11 @@ import com.bumptech.glide.module.AppGlideModule
  **/
 @GlideModule
 class CybirdGlideModule : AppGlideModule() {
-    override fun applyOptions(context: Context?, builder: GlideBuilder?) {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
         super.applyOptions(context, builder)
         //设置内存缓存 30MB
         val MAX_MEMORY_CACHE_SIZE = 1024 * 1024 * 30 //30MB
-        builder?.setMemoryCache(LruResourceCache(MAX_MEMORY_CACHE_SIZE.toLong()))
+        builder.setMemoryCache(LruResourceCache(MAX_MEMORY_CACHE_SIZE.toLong()))
 
         //硬盘缓存
         val MAX_DISK_CACHE_SIZE = 1024 * 1024 * 100 //100MB
@@ -32,14 +34,17 @@ class CybirdGlideModule : AppGlideModule() {
             val downloadDirectoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
                     CACHE_FILE_NAME
             //路径---->sdcard/imgCache
-            builder?.setDiskCache(DiskLruCacheFactory(downloadDirectoryPath, MAX_DISK_CACHE_SIZE.toLong()))
+            builder.setDiskCache(DiskLruCacheFactory(downloadDirectoryPath, MAX_DISK_CACHE_SIZE.toLong()))
         } else {
             //路径---->/sdcard/Android/data/<application package>/cache/imgCache
-            builder?.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, CACHE_FILE_NAME, MAX_DISK_CACHE_SIZE.toLong()))
+            builder.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, CACHE_FILE_NAME, MAX_DISK_CACHE_SIZE.toLong()))
         }
+
+        //设置默认请求策略
+        builder.setDefaultRequestOptions(RequestOptions().format(DecodeFormat.PREFER_RGB_565).disallowHardwareConfig())
     }
 
-    override fun registerComponents(context: Context?, glide: Glide?, registry: Registry?) {
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         super.registerComponents(context, glide, registry)
     }
 
