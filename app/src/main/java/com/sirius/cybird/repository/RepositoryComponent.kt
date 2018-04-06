bc9e435fe9f30741ae49288b06b0b689a660d392
Botasky <291770865@qq.com>
2018-04-06 20:03:14 +0800

    update for dagger

--- app/src/main/java/com/sirius/cybird/repository/RepositoryComponent.kt
+++ app/src/main/java/com/sirius/cybird/repository/RepositoryComponent.kt
@@ -1,10 +1,12 @@
 package com.sirius.cybird.repository
 
+import com.google.gson.Gson
 import com.sirius.cybird.di.model.ApplicationModule
 import com.sirius.cybird.di.model.NetModule
 import com.sirius.cybird.module.TestInject
 import com.sirius.cybird.ui.home.HomePresenter
 import dagger.Component
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 
@@ -19,5 +21,8 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = arrayOf(ApplicationModule::class, NetModule::class))
 interface RepositoryComponent{
-    fun getHomePresenter():HomePresenter //对单个类进行inject,表示对此类公开可见
+//    fun getHomePresenter():HomePresenter //对单个类进行inject,表示对此类公开可见
+    fun getRetorofit():Retrofit  //表示开放Module中的依赖
+
+    fun getGson():Gson
 }
--- app/src/main/java/com/sirius/cybird/ui/home/HomePresenter.kt
+++ app/src/main/java/com/sirius/cybird/ui/home/HomePresenter.kt
@@ -1,6 +1,7 @@
 package com.sirius.cybird.ui.home
 
 import android.util.Log
+import com.google.gson.Gson
 import com.sirius.cybird.db.FilmEntity
 import com.sirius.cybird.net.api.FilmsApi
 import com.sirius.cybird.net.url.Urls
@@ -12,23 +13,27 @@ import javax.inject.Inject
 
 class HomePresenter {
     val mRetrofit: Retrofit
+    val mGson: Gson
 
     @Inject
-    constructor(retrofit: Retrofit){
+    constructor(retrofit: Retrofit, gson: Gson) {
         mRetrofit = retrofit
+        mGson = gson
     }
 
-    fun getTime(){
+    fun getTime() {
         mRetrofit.create(FilmsApi::class.java)
                 .getInTheaters(Urls.DOUBAN_FILM_URL_HOST + "movie/in_theaters")
                 .flatMap { filmsData -> Observable.fromIterable(filmsData.films) }
                 .compose(TransformScheduler.applyNewThreadScheduler())
-                .subscribe({ film ->
-                    val filmEntity = FilmEntity(text = film.title, comment = film.originalTitle, date = Date())
+                .subscribe(
+                        { film ->
+                            val filmEntity = FilmEntity(text = film.title, comment = film.originalTitle, date = Date())
 
-                    Log.e("TAG", "put success " + filmEntity.text)
-                }, { e ->
-                    Log.e("TAG", e.message)
-                })
+                            Log.e("TAG", "put success " + mGson.toJson(filmEntity))
+                        },
+                        { e ->
+                            Log.e("TAG", e.message)
+                        })
     }
 }
