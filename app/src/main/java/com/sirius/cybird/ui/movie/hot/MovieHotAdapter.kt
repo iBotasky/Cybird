ec5d2cff5894d2e7fd08b0d598c30aea6e2ec005
Botasky <291770865@qq.com>
2018-04-18 22:18:48 +0800

    remove databinding for adapter

--- app/src/main/java/com/sirius/cybird/ui/movie/hot/MovieHotAdapter.kt
+++ app/src/main/java/com/sirius/cybird/ui/movie/hot/MovieHotAdapter.kt
@@ -1,40 +1,26 @@
 package com.sirius.cybird.ui.movie.hot
 
 
+
 import android.support.annotation.Nullable
-import android.view.View
-import android.view.ViewGroup
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
-import com.sirius.cybird.BR
 import com.sirius.cybird.R
-import com.sirius.cybird.databinding.ItemFilmBinding
 import com.sirius.cybird.net.response.Film
 import com.sirius.cybird.utils.GlideApp
 
 
-class MovieHotAdapter(@param:Nullable private val films: List<Film>) : BaseQuickAdapter<Film, MovieHotAdapter.MovieViewHolder>(R.layout.item_film, films) {
-    override fun convert(helper: MovieViewHolder, item: Film) {
-        val binding = helper.binding
+class MovieHotAdapter(@param:Nullable private val films: List<Film>) : BaseQuickAdapter<Film, BaseViewHolder>(R.layout.item_film, films) {
+    override fun convert(helper: BaseViewHolder, item: Film) {
         GlideApp.with(this.mContext)
                 .load("https://www.baidu.com/img/superlogo_c4d7df0a003d3db9b65e9ef0fe6da1ec.png")
                 .placeholder(R.drawable.img_holder)
                 .centerCrop()
                 .into(helper.getView(R.id.iv_film_img))
-        binding.setVariable(BR.item, item)
-        binding.executePendingBindings()
-    }
 
+        helper.setText(R.id.film_name, item.title)
+        helper.setText(R.id.tv_score, item.rating.average.toString())
 
-    override fun getItemView(layoutResId: Int, parent: ViewGroup): View {
-        val binding: ItemFilmBinding = ItemFilmBinding.inflate(mLayoutInflater, parent, false)
-        val view = binding.root
-        view.setTag(R.id.id_tag_movie, binding)
-        return view
-    }
-
-    class MovieViewHolder(view: View) : BaseViewHolder(view) {
-        val binding: ItemFilmBinding
-            get() = itemView.getTag(R.id.id_tag_movie) as ItemFilmBinding
+        helper.setRating(R.id.film_rating_bar, (item.rating.average / item.rating.max * 5.0f).toFloat())
     }
 }
