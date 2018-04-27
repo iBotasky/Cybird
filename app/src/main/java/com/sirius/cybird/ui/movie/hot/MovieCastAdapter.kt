2b10b66e74915cbc2589c6947a713e882648c3e1
Botasky <291770865@qq.com>
2018-04-27 22:31:49 +0800

    add:Film items add a recyclerview for cast

---
+++ app/src/main/java/com/sirius/cybird/ui/movie/hot/MovieCastAdapter.kt
@@ -0,0 +1,37 @@
+package com.sirius.cybird.ui.movie.hot
+
+import android.databinding.DataBindingUtil
+import android.view.View
+import android.view.ViewGroup
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.sirius.cybird.BR
+import com.sirius.cybird.R
+import com.sirius.cybird.databinding.ItemFilmCastsBinding
+import com.sirius.cybird.net.response.Cast
+import com.sirius.cybird.utils.GlideUtil
+import org.jetbrains.annotations.NotNull
+
+class MovieCastAdapter(@NotNull casts: List<Cast>) : BaseQuickAdapter<Cast, MovieCastAdapter.ViewHolder>(R.layout.item_film_casts, casts) {
+    override fun convert(helper: ViewHolder, item: Cast) {
+        val binding = helper.binding
+        GlideUtil.loadImage(mContext, binding.ivAvatar, item.avatars.small)
+        binding.setVariable(BR.name, item.name)
+        binding.executePendingBindings()
+    }
+
+
+    override fun getItemView(layoutResId: Int, parent: ViewGroup): View {
+        //有loadmore的时候需要return super.getItemView(layoutResId, parent)
+        val binding: ItemFilmCastsBinding = DataBindingUtil.inflate(mLayoutInflater, layoutResId, parent, false)
+                ?: return super.getItemView(layoutResId, parent)
+        val view = binding.root
+        view.setTag(R.id.id_tag_movie_cast, binding)
+        return view
+    }
+
+    class ViewHolder(view: View) : BaseViewHolder(view) {
+        val binding: ItemFilmCastsBinding
+            get() = itemView.getTag(R.id.id_tag_movie_cast) as ItemFilmCastsBinding
+    }
+}
--- app/src/main/java/com/sirius/cybird/ui/movie/hot/MovieHotAdapter.kt
+++ app/src/main/java/com/sirius/cybird/ui/movie/hot/MovieHotAdapter.kt
@@ -2,9 +2,10 @@ package com.sirius.cybird.ui.movie.hot
 
 
 import android.databinding.DataBindingUtil
-import android.support.annotation.Nullable
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.ViewGroup
+import com.blankj.utilcode.util.LogUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.sirius.cybird.BR
@@ -12,15 +13,19 @@ import com.sirius.cybird.R
 import com.sirius.cybird.databinding.ItemFilmBinding
 import com.sirius.cybird.net.response.Film
 import com.sirius.cybird.utils.GlideUtil
+import com.sirius.cybird.utils.divider.HorizontalSpaceDecoration
 
 
 class MovieHotAdapter : BaseQuickAdapter<Film, MovieHotAdapter.ViewHolder>(R.layout.item_film) {
     override fun convert(helper: ViewHolder, item: Film) {
         val binding = helper.binding
         GlideUtil.loadImage(mContext, binding.ivFilmImg, item.images.small)
-        binding.setVariable(BR.title, item.title)
-        binding.setVariable(BR.rating, item.rating.average.toString())
-        binding.setVariable(BR.stars, (item.rating.average * 5.0f / item.rating.max).toFloat())
+        val rvCasts = binding.rvCast
+        LogUtils.e("cast size " + item.casts.size)
+        rvCasts.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false)
+        rvCasts.adapter = MovieCastAdapter(item.casts)
+        if (rvCasts.itemDecorationCount == 0)
+            rvCasts.addItemDecoration(HorizontalSpaceDecoration(R.dimen.dimen_0, R.dimen.dimen_0, R.dimen.dp_10))
         var directors: String = ""
         for (director in item.directors) {
             if (item.directors.indexOf(director) != item.directors.size - 1)
@@ -29,13 +34,17 @@ class MovieHotAdapter : BaseQuickAdapter<Film, MovieHotAdapter.ViewHolder>(R.lay
                 directors += director.name
         }
         binding.setVariable(BR.director, mContext.getString(R.string.movie_director, directors))
+        binding.setVariable(BR.title, item.title)
+        binding.setVariable(BR.rating, item.rating.average.toString())
+        binding.setVariable(BR.stars, (item.rating.average * 5.0f / item.rating.max).toFloat())
         binding.executePendingBindings()
     }
 
 
     override fun getItemView(layoutResId: Int, parent: ViewGroup): View {
         //有loadmore的时候需要return super.getItemView(layoutResId, parent)
-        val binding: ItemFilmBinding = DataBindingUtil.inflate(mLayoutInflater, layoutResId, parent, false) ?: return super.getItemView(layoutResId, parent)
+        val binding: ItemFilmBinding = DataBindingUtil.inflate(mLayoutInflater, layoutResId, parent, false)
+                ?: return super.getItemView(layoutResId, parent)
         val view = binding.root
         view.setTag(R.id.id_tag_movie, binding)
         return view
