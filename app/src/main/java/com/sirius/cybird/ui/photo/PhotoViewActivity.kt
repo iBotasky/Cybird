655daadba70a0253178985b9a8110523ecec4418
Botasky <291770865@qq.com>
2018-05-18 15:06:46 +0800

    update:图片保存操作

--- app/src/main/java/com/sirius/cybird/ui/photo/PhotoViewActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/photo/PhotoViewActivity.kt
@@ -4,10 +4,14 @@ import android.support.v4.view.ViewPager
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import com.blankj.utilcode.util.ToastUtils
 import com.sirius.cybird.R
 import com.sirius.cybird.databinding.ActivityPhotoViewBinding
+import com.sirius.cybird.rx.TransformScheduler
 import com.sirius.cybird.ui.Navigation
 import com.sirius.cybird.ui.base.BaseActivity
+import com.sirius.cybird.utils.GlideUtil
+import io.reactivex.Observable
 
 /**
  *Created by Botasky on 2018/5/12
@@ -18,7 +22,7 @@ class PhotoViewActivity : BaseActivity() {
     lateinit var mPhotos: List<String>
 
     val mToolbarDismissAnimation: Animation = AlphaAnimation(1.0f, 0.0f)
-    val mToolbarDisplayAnimation:Animation = AlphaAnimation(0.0f, 1.0f)
+    val mToolbarDisplayAnimation: Animation = AlphaAnimation(0.0f, 1.0f)
 
     var mCurrentIndex = 0
 
@@ -32,37 +36,56 @@ class PhotoViewActivity : BaseActivity() {
         mPhotoViewModel = PhotoViewModel(mCurrentIndex, mPhotos.size)
         mPhotoViewBinding.photo = mPhotoViewModel
         setupViewPager()
+        setupDownload()
+    }
+
+    private fun setupDownload() {
+        mPhotoViewBinding.ivDownload.setOnClickListener({
+            Observable.just(mPhotos[mCurrentIndex])
+                    .flatMap { url -> GlideUtil.saveImage(this, url) }
+                    .compose(TransformScheduler.applyNewThreadScheduler())
+                    .compose(bindToLifecycle())
+                    .subscribe(
+                            { s -> ToastUtils.showShort(s) },
+                            { e -> ToastUtils.showShort(e.message) },
+                            {}
+                    )
+        })
     }
 
     private fun setupViewPager() {
         mToolbarDisplayAnimation.duration = 100
         mToolbarDismissAnimation.duration = 100
 
-        mToolbarDisplayAnimation.setAnimationListener(object :Animation.AnimationListener{
+        mToolbarDisplayAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationRepeat(animation: Animation?) {
             }
+
             override fun onAnimationEnd(animation: Animation?) {
                 mToolbar?.visibility = View.VISIBLE
             }
+
             override fun onAnimationStart(animation: Animation?) {
             }
         })
-        mToolbarDismissAnimation.setAnimationListener(object : Animation.AnimationListener{
+        mToolbarDismissAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationRepeat(animation: Animation?) {
             }
+
             override fun onAnimationEnd(animation: Animation?) {
                 mToolbar?.visibility = View.INVISIBLE
             }
+
             override fun onAnimationStart(animation: Animation?) {
             }
         })
 
 
-        mPhotoViewBinding.idViewPager.adapter = PhotoViewAdapter(mPhotos, this, object :PhotoViewAdapter.Listener{
+        mPhotoViewBinding.idViewPager.adapter = PhotoViewAdapter(mPhotos, this, object : PhotoViewAdapter.Listener {
             override fun callback() {
-                if (mToolbar?.visibility == View.INVISIBLE){
+                if (mToolbar?.visibility == View.INVISIBLE) {
                     mToolbar?.startAnimation(mToolbarDisplayAnimation)
-                }else{
+                } else {
                     mToolbar?.startAnimation(mToolbarDismissAnimation)
                 }
             }
@@ -76,6 +99,7 @@ class PhotoViewActivity : BaseActivity() {
             }
 
             override fun onPageSelected(position: Int) {
+                mCurrentIndex = position
                 mPhotoViewModel.index = position
             }
         })
--- app/src/main/java/com/sirius/cybird/utils/GlideUtil.kt
+++ app/src/main/java/com/sirius/cybird/utils/GlideUtil.kt
@@ -1,18 +1,19 @@
 package com.sirius.cybird.utils
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
 import android.widget.ImageView
+import com.blankj.utilcode.util.FileUtils
 import com.sirius.cybird.R
+import io.reactivex.Observable
 import java.io.File
-import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.FutureTarget
-
 
 
 object GlideUtil {
 
-    fun loadImage(context: Context, imageview: ImageView, url: String){
+    fun loadImage(context: Context, imageview: ImageView, url: String) {
         GlideApp.with(context)
                 .load(url)
                 .placeholder(R.drawable.img_holder)
@@ -21,7 +22,7 @@ object GlideUtil {
                 .into(imageview)
     }
 
-    fun loadImageNotCrop(context: Context, imageview: ImageView, url: String){
+    fun loadImageNotCrop(context: Context, imageview: ImageView, url: String) {
         GlideApp.with(context)
                 .load(url)
                 .placeholder(R.drawable.img_holder)
@@ -36,12 +37,36 @@ object GlideUtil {
      * submit()方法也是可以替换into()方法的，不过submit()方法的用法明显要比preload()方法复杂不少。
      * 这个方法只会下载图片，而不会对图片进行加载。当图片下载完成之后，我们可以得到图片的存储路径，以便后续进行操作。
      */
-    fun getFile(context: Context, url: String): File {
+    //TODO 后缀问题
+    fun saveImage(context: Context, url: String): Observable<String> {
+        val regex = "(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|png|jpeg)".toRegex()
+        if (!url.matches(regex)) {
+            return Observable.just(context.getString(R.string.girl_formate_not_allow))
+        }
         val future = GlideApp
                 .with(context)
                 .asFile()
                 .load(url)
                 .submit()
-        return future.get()
+        var file = future.get()
+
+        // 首先保存图片
+        val pictureFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsoluteFile()
+
+        val appDir = File(pictureFolder, "Cybird")
+        if (!appDir.exists()) {
+            appDir.mkdirs()
+        }
+        val fileName = System.currentTimeMillis().toString() + ".jpg"
+        val destFile = File(appDir, fileName)
+
+        FileUtils.copyFile(file, destFile, object : FileUtils.OnReplaceListener {
+            override fun onReplace(): Boolean {
+                return true
+            }
+        })
+        // 最后通知图库更新
+        context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(File(destFile.path))))
+        return Observable.just(context.getString(R.string.girl_success))
     }
 }
