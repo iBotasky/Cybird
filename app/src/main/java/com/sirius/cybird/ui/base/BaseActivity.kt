d14eec27756376948ed7fbe6daace53a04ef192c
Botasky <291770865@qq.com>
2019-01-22 14:50:48 +0800

    update for gradle and some kotlin code

--- app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/base/BaseActivity.kt
@@ -11,7 +11,6 @@ import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
-import com.gyf.barlibrary.BarHide
 import com.gyf.barlibrary.ImmersionBar
 import com.kennyc.view.MultiStateView
 import com.sirius.cybird.CybirdApp
--- app/src/main/java/com/sirius/cybird/ui/main/MainActivity.kt
+++ app/src/main/java/com/sirius/cybird/ui/main/MainActivity.kt
@@ -11,11 +11,19 @@ import com.sirius.cybird.ui.home.HomeActivity
 import com.sirius.cybird.ui.base.BaseActivity
 import io.objectbox.Box
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
 
 /**
  * Create by Botasky
  */
-class MainActivity : BaseActivity() {
+
+enum class Delivery {
+    STANDARD, EXPEDITED
+}
+
+
+class MainActivity : BaseActivity(),AnkoLogger {
     private lateinit var mFilmBox: Box<FilmEntity>
     private lateinit var mMainBinding: ActivityMainBinding
 
@@ -27,8 +35,13 @@ class MainActivity : BaseActivity() {
         mMainBinding.viewModel = viewModel
         mMainBinding.executePendingBindings()
         mFilmBox = CybirdApp.getBoxStore().boxFor(FilmEntity::class.java)
-        button.setOnClickListener(({
+        button.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
+            toast("""${max(20, 30)} , default ${default(a = 2)}""")
+            withTest()
+            applyTest()
+            letTest()
+            error(getShippingCostCalculator(Delivery.EXPEDITED)(5f))
 //            FilmsApi.getFilmsService()
 //                    .getComingSoon(0, 10)
 //                    .flatMap { filmsData -> Observable.fromIterable(filmsData.films) }
@@ -41,7 +54,7 @@ class MainActivity : BaseActivity() {
 //                    }, { e ->
 //                        Log.e("TAG", e.message)
 //                    })
-        }))
+        }
     }
 
     override fun getLayoutResource(): Int {
@@ -49,5 +62,40 @@ class MainActivity : BaseActivity() {
     }
 
     override fun initializeInjector() {
+    }
+
+
+    fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+    fun default(a: Int = 2, b: Int = 33) = if (a > b) a else b
+
+    fun withTest() {
+        val string = "Arg"
+        val newString = with(string) {
+            this.plus("mm")
+        }
+        error(newString)
+    }
+
+    fun applyTest() = "mMM".apply {
+        error(this.length)
+    }
+
+    fun letTest() {
+        val str: String = "Arg"
+        val result = str.let {
+            error(this) // Receiver this指MainActivity，
+            error(it) // Argument
+            42 // Block return value
+        }
+        error(result)
+    }
+
+    //函数作为返回值，返回的是一个用Float为参数，Double为返回值得函数计算，
+    fun getShippingCostCalculator(delivery: Delivery): (Float) -> Double {
+        if (delivery == Delivery.EXPEDITED) {
+            return { 6 + 2.1 * it }
+        }
+        return { 1.3 * it }
     }
 }
